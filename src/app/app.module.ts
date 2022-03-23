f63500e05dd4c9ebb277e2aee483b76f330d23ab
saul <saul@.hotmail.com>
2022-03-23 11:51:01 -0500

    creando estructura de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MarcadorComponent } from './marcador/marcador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcadorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/marcador/marcador.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marcador',
+  templateUrl: './marcador.component.html',
+  styleUrls: ['./marcador.component.css']
+})
+export class MarcadorComponent implements OnInit {
+
+  puntosLocales=0;
+  puntosVisintantes=0;
+
+  equipoLocal:any ={
+    nombre:'lakers',
+    logo: '/assets/img/LAL.svg',
+    jugadores:[
+      {nombre:'saul',puntos:0,imagen:'/assets/img/thomas.png'},
+      {nombre:'carlos',puntos:0,imagen:'/assets/img/kuzma.png'},
+      {nombre:'thiago',puntos:0,imagen:'/assets/img/randle.png'},
+      {nombre:'miguel',puntos:0,imagen:'/assets/img/ingram.png'},
+      {nombre:'lopez',puntos:0,imagen:'/assets/img/lopez.png'},
+    ]
+  }
+
+  equipoVisitante:any ={
+    nombre:'celtics',
+    logo: '/assets/img/BOS.svg',
+    jugadores:[
+      {nombre:'andres',puntos:0,imagen:'/assets/img/allen.png'},
+      {nombre:'julio',puntos:0,imagen:'/assets/img/bynes.png'},
+      {nombre:'verto',puntos:0,imagen:'/assets/img/bird.png'},
+      {nombre:'pedro',puntos:0,imagen:'/assets/img/brown.png'},
+      {nombre:'jhon',puntos:0,imagen:'/assets/img/gibson.png'},
+    ]
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
